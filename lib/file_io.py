43731f444df9e493de998e936ad14fd8a7dbde90
Victor Njoroge <victornjoroge2020@gmail.com>
2023-09-02 11:24:33 +0300

    fone

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name="/home/victor/python-p3-fileio-lab/lib/text.txt", file_content="Log 1: 5 bananas added"):
+    with open(file_name, mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
+#write_file()
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name="/home/victor/python-p3-fileio-lab/lib/text.txt", append_content="Log 2: 3 bananas subtracted"):
+    with open(file_name, mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
+#append_file()
 
-def read_file(file_name):
-    pass
+def read_file(file_name="/home/victor/python-p3-fileio-lab/lib/text.txt"):
+    with open(file_name, mode='r', encoding='utf-8')as log_file:
+        for line in log_file:
+            print(line)
+read_file()
